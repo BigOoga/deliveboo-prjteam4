5da6fff10e63589e53d7445cafa354727da5cc84
Alessandro2014 <alessandromerenda22@gmail.com>
2021-11-05 15:20:22 +0100

    order and dish migration

---
+++ database/migrations/2021_11_05_140850_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('status');
+            $table->string('address');
+            $table->string('user_name', 50);
+            $table->string('user_surname', 50);
+            $table->string('phone', 15);
+            $table->string('email', 50);
+            $table->float('total', 7,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
