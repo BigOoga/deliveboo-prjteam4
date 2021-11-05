e51950915651ed87096d45fa7d5d2c9632d67a01
Alessandro2014 <alessandromerenda22@gmail.com>
2021-11-05 11:02:57 +0100

    model migration

---
+++ database/migrations/2021_11_05_100123_create_restaurants_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique();
+            $table->string('email',50)->unique();
+            $table->string('password');
+            $table->string('address');
+            $table->string('iva')->unique();
+            $table->string('image')->nullable();
+            $table->unsignedInteger('delivery_fee');
+            $table->tinyInteger('votes')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('is_open');
+            $table->time('opening_time');
+            $table->time('closing_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
