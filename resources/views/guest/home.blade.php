6bd7ecc76a501c0617c98a14e65f733beb58dbb1
maldrek3103 <cpisanu96@gmail.com>
2021-11-08 15:50:18 +0100

    added navbar

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -13,7 +13,32 @@
     <body>
        
             <div id="root">
-                Hello
+
+                {{-- Header --}}
+                <header>
+                    <!-- Image and text -->
+                    <nav class="navbar navbar-light d-flex justify-content-space-between container">
+                        <div>
+                            <a class="navbar-brand" href="#">
+                                <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="Logo">
+                                Deliveboo
+                            </a>
+                        </div>
+                        <div>
+                            <a href="#">carrello</a>
+                            <a href="#">accedi</a>
+                            <a href="#">menu</a>
+                        </div>
+                    </nav>
+                </header>
+
+                {{-- Main --}}
+                <main></main>
+
+                {{-- Footer --}}
+                <footer></footer>
+
+
             </div>
 
             <script src="{{ asset('js/front.js')}}">
