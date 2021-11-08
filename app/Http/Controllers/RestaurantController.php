c93647658d52505e8e01adbd6d6611071fa57079
ResminiLoris <resminiloris@gmail.com>
2021-11-08 10:26:05 +0100

    gestito index ristoranti

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
+
 
 class RestaurantController extends Controller
 {
@@ -13,7 +15,8 @@ class RestaurantController extends Controller
      */
     public function index()
     {
-        //
+        $restaurants= Restaurant::all();
+        return view('restaurants.index', compact('restaurants'));
     }
 
     /**
