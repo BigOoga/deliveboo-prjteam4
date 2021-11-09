6cfc39b6c8ac1b95588241a26ca08286bcdde26d
maldrek3103 <cpisanu96@gmail.com>
2021-11-09 16:01:59 +0100

    added logo and fixed typos

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Boolpress</title>
+        <link rel="icon" type="image/png" href="{{ asset('images/logo-small.png') }}"/>
+        <title>Deliveboo | Home</title>
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
 
@@ -22,7 +22,7 @@
                             <div>
                                 <a class="navbar-brand" href="#">
                                     <span class="text-white">
-                                        <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="Logo">
+                                        <img src="{{ asset('images/logo-small.png') }}" width="30" height="30" class="d-inline-block align-top" alt="Logo">
                                         Deliveboo
                                     </span>
                                 </a>
