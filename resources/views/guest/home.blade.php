3a6540e2d1005441d0e7f98238284d4f6f304b4a
maldrek3103 <cpisanu96@gmail.com>
2021-11-09 11:38:41 +0100

    added more main default with comments

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -58,52 +58,122 @@
 
                 {{-- Main --}}
                 <main>
+
+                    {{-- Made first main section, need to make it respondive --}}
                     <div id="deliveboo-section" class="my-3">
                         <div class="container">
+                            {{-- Section title --}}
                         <h2 class="mb-3">La selezione di Deliveboo</h2>
                             <div class="row">
                               <div class="col-5 d-flex py-3">
                                 <div class="card flex-grow-1" style="width: 18rem;">
+                                    {{-- Card image --}}
                                     <img src="..." class="card-img-top" alt="...">
                                     <div class="card-body">
+                                        {{-- Card title --}}
                                       <h5 class="card-title">Comfort food</h5>
+                                      {{-- Description --}}
                                       <p class="card-text">I grandi classici che scaldano il cuore, perfetti in ogni momento.</p>
+                                      {{-- Link to card object --}}
                                       <a href="#">Scopri Comfort Food</a>
                                     </div>
                                   </div>
                               </div>
                               <div class="col-7 d-flex py-3">
                                 <div class="card flex-grow-1" style="width: 18rem;">
+                                    {{-- Card image --}}
                                     <img src="..." class="card-img-top" alt="...">
                                     <div class="card-body">
+                                        {{-- Card title --}}
                                       <h5 class="card-title">Dolci e dessert</h5>
+                                      {{-- Description --}}
                                       <p class="card-text">Dolci piaceri per rendere la giornata ancora più gustosa.</p>
+                                      {{-- Link to card object --}}
                                       <a href="#">Scopri Dolci e dessert</a>
                                     </div>
                                   </div>
                               </div>
                               <div class="col-7 d-flex py-3">
                                 <div class="card flex-grow-1" style="width: 18rem;">
+                                    {{-- Card image --}}
                                     <img src="..." class="card-img-top" alt="...">
                                     <div class="card-body">
+                                        {{-- Card title --}}
                                       <h5 class="card-title">Perfetti da condividere</h5>
+                                      {{-- Description --}}
                                       <p class="card-text">Serve una scusa per stare insieme? Ordina dai ristoranti che trasformeranno la tua serata in un vera festa.</p>
+                                      {{-- Link to card object --}}
                                       <a href="#">Scopri Perfetti da condividere</a>
                                     </div>
                                   </div>
                               </div>
                               <div class="col-5 d-flex py-3">
                                 <div class="card flex-grow-1" style="width: 18rem;">
+                                    {{-- Card image --}}
                                     <img src="..." class="card-img-top" alt="...">
                                     <div class="card-body">
+                                        {{-- Card title --}}
                                       <h5 class="card-title">Esclusiva deliveroo</h5>
+                                      {{-- Description --}}
                                       <p class="card-text">I più famosi, i più buoni, i preferiti. Quelli che trovi solo su Deliveboo.</p>
+                                      {{-- Link to card object --}}
                                       <a href="#">Scopri Esclusiva Deliveboo</a>
                                     </div>
                                   </div>
                               </div>
                             </div>
                           </div>
+                    </div>
+
+
+                    {{-- Da moltiplicare le cards e aggiustare le desc --}}
+                    <div id="default-section" class="my-3">
+                        <div class="container">
+                            {{-- Section title --}}
+                        <h2 class="mb-3">La selezione di Deliveboo</h2>
+                            <div class="row">
+                              <div class="col-4 d-flex py-3">
+                                <div class="card flex-grow-1" style="width: 18rem;">
+                                    {{-- Card image --}}
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        {{-- Card title --}}
+                                      <h5 class="card-title">Comfort food</h5>
+                                      {{-- Description --}}
+                                      <p class="card-text">I grandi classici che scaldano il cuore, perfetti in ogni momento.</p>
+                                    </div>
+                                  </div>
+                              </div>
+                              <div class="col-4 d-flex py-3">
+                                <div class="card flex-grow-1" style="width: 18rem;">
+                                    {{-- Card image --}}
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        {{-- Card title --}}
+                                      <h5 class="card-title">Dolci e dessert</h5>
+                                      {{-- Description --}}
+                                      <p class="card-text">Dolci piaceri per rendere la giornata ancora più gustosa.</p>
+                                    </div>
+                                  </div>
+                              </div>
+                              <div class="col-4 d-flex py-3">
+                                <div class="card flex-grow-1" style="width: 18rem;">
+                                    {{-- Card image --}}
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        {{-- Card title --}}
+                                      <h5 class="card-title">Perfetti da condividere</h5>
+                                      {{-- Description --}}
+                                      <p class="card-text">Serve una scusa per stare insieme? Ordina dai ristoranti che trasformeranno la tua serata in un vera festa.</p>
+                                    </div>
+                                  </div>
+                              </div>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div>
+
                     </div>
                 </main>
 
