4f61c54137dc94e05d2f1218bc5f024aa8be934d
maldrek3103 <cpisanu96@gmail.com>
2021-11-09 17:05:50 +0100

    improved sidebar style

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -36,8 +36,12 @@
                             {{-- Sidebar --}}
                             <div id="mySidenav" class="sidenav">
                                 <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                                <a class="btn btn-light" href="{{ route('login') }}" role="button">Accedi o registrati</a>
+                                <span class="d-flex mx-3">
+                                    <img src="{{ asset('images/logo-small.png') }}" width="30" height="30" alt="Deliveboo">
+                                    <h4 class="mx-3">Deliveboo</h4>
+                                </span>
                                 <hr>
+                                <a class="btn btn-primary mx-3" href="{{ route('login') }}" role="button">Accedi o registrati</a>
                                 <a href="{{ route('restaurants.create') }}">Aggiungi il tuo ristorante</a>
                               </div>
                         </nav>
@@ -50,7 +54,7 @@
                                 <div class="col-6">
                                     <form class="form-inline mr-auto d-flex">
                                         <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-                                        <button class="btn btn-mdb-color btn-rounded btn-sm my-0 ml-sm-2" type="submit">Search</button>
+                                        <button class="btn btn-rounded btn-sm my-0 ml-sm-2" type="submit">Search</button>
                                     </form>
                                 </div>
                                 <div class="col-6">
@@ -229,41 +233,39 @@
                     background-color: #fff;
                     overflow-x: hidden;
                     transition: 0.5s;
-                    padding-top: 60px;
+                    padding-top: 1rem;
+                    border-left: 1px solid #eee;
                 }
 
                 .sidenav a {
                     padding: 8px 8px 8px 32px;
                     text-decoration: none;
-                    font-size: 25px;
+                    font-size: 1rem;
                     color: #000;
                     display: block;
                     transition: 0.3s;
                 }
 
                 .sidenav a:hover {
-                    color: #f1f1f1;
+                    color: #ccc;
                 }
 
                 .sidenav .closebtn {
                     position: absolute;
                     top: 0;
                     right: 25px;
-                    font-size: 36px;
+                    font-size: 2rem;
                     margin-left: 50px;
                 }
 
-                @media screen and (max-height: 450px) {
-                    .sidenav {padding-top: 15px;}
-                    .sidenav a {font-size: 18px;}
-                }
+                
             </style>
 
 
             {{-- Scripts --}}
             <script>
                 function openNav() {
-                    document.getElementById("mySidenav").style.width = "250px";
+                    document.getElementById("mySidenav").style.width = "375px";
                 }
 
                 function closeNav() {
