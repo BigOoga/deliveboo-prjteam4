9a9509e5810e3e1c2ec7ffabf5f5476180000336
Alessandro2014 <alessandromerenda22@gmail.com>
2021-11-09 10:52:24 +0100

    layouts

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('guest.home');
+    return view('guest/home');
 });
 
 Route::resource('restaurants', 'RestaurantController');
