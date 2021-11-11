97679cefbedd06859aa86659a0a14da3e741886d
BigOoga <edoardo.ruffi@gmail.com>
2021-11-11 14:28:37 +0100

    created a basic restaurants.index page

---
+++ resources/views/restaurants/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Deliveboo</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+    <div id="app">
+        <topbar></topbar>
+        <div class="container-fluid ">
+            <div class="row">
+                <div class="col-2 border">
+                    <sidebar></sidebar>
+                </div>
+                <div class="col-10 border">search results</div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
