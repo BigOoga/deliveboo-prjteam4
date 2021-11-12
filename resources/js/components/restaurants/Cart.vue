2cb2f94fae794c50cc72165d9fd7c71cdb7f7008
ResminiLoris <resminiloris@gmail.com>
2021-11-12 12:46:24 +0100

    base struttura carrello

--- resources/js/components/restaurants/Cart.vue
+++ resources/js/components/restaurants/Cart.vue
@@ -1,17 +1,46 @@
 <template>
-    <div class="row">
+    <div class="row border p-5" id="main-row">
         <div class="col-12">
             <h4>Il tuo ordine</h4>
+            <!-- qui ci andranno i prodotti selezionati -->
+        </div>
+        <hr>
+        <!-- subtotal -->
+        <div class="col-12 d-flex justify-content-between">
+            <span>subtotale</span>
+            <span>12.90$</span>
+        </div>
+        <!-- delivery fee -->
+        <div class="col-12 d-flex justify-content-between">
+            <span>spese di consegna</span>
+            <span>2.50$</span>
+        </div>
+        <hr class="mt-2 mb-2">
+        <!-- total -->
+        <div class="col-12 d-flex justify-content-between">
+            <span>Total</span>
+            <span>124.50$</span>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Cart"
+    name: "Cart",
+    props:[],
+    data() {
+        return {
+           
+        };
+    },
+    methods: {
+       
+    },
 };
 </script>
 
 <style lang="scss" scoped>
-
+    #main-row{
+    
+    }
 </style>
--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -4,54 +4,63 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    <div class="container">   
-        {{-- RESTAURANT DETAILS  --}}
-        <h1>PROVA TEST</h1>
+    <div class="container mt-5 mb-5">  
         <div class="row">
-            {{-- RESTAURANT PIC  --}}
-            <div>
-                <img src="{{ asset('storage/' . $restaurant->image) }}" alt="{{ $restaurant->name }}" class="img-fluid rounded px-3" style="max-width: 450px;"> 
+            {{-- RESTAURANT DETAILS  --}}
+            <h1>PROVA TEST</h1>
+            <div class="row">
+                {{-- RESTAURANT PIC  --}}
+                <div>
+                    <img src="{{ asset('storage/' . $restaurant->image) }}" alt="{{ $restaurant->name }}" class="img-fluid rounded px-3" style="max-width: 450px;"> 
+                </div>
+                <div>
+                    <h1>{{$restaurant->name}}</h1>
+                {{-- RESTAURANT INFO  --}}
+                    <ul class="list-unstyled">
+                        <li><time>Orario di apertura {{$restaurant->opening_time}}</time></li>
+                        <li><time>Orario di chiusura {{$restaurant->closing_time}}</time></li>
+                        <li>Contatti: {{$restaurant->email}}</li>
+                        <li>Spese di spedizione: € {{$restaurant->delivery_fee}}</li>
+                        {{-- INSERT DESCRIPTION  --}}
+                    </ul>
+                </div>
             </div>
-            <div>
-                <h1>{{$restaurant->name}}</h1>
-            {{-- RESTAURANT INFO  --}}
-                <ul class="list-unstyled">
-                    <li><time>Orario di apertura {{$restaurant->opening_time}}</time></li>
-                    <li><time>Orario di chiusura {{$restaurant->closing_time}}</time></li>
-                    <li>Contatti: {{$restaurant->email}}</li>
-                    <li>Spese di spedizione: € {{$restaurant->delivery_fee}}</li>
-                    {{-- INSERT DESCRIPTION  --}}
-                </ul>
-            </div>
-        </div>
-
-        {{-- MENU  --}}
-        <h1>Menu</h1>
-        <div class="row">
-            @foreach($dishes as $dish) 
-                <div class="card m-3 shadow" style="max-width: 500px;">
-                    <div class="row g-0">
-                        <div class="col-4">
-                            <img src="{{ asset('storage/' . $dish->picture) }}" class="img-fluid rounded-start" alt="{{$dish->name}}">
-                        </div>
-                        <div class="col-8">
-                            <div class="card-body">
-                                <h5 class="card-title">{{$dish->name}}</h5>
-                                <p class="card-text">{{ $dish->description}}</p>
+            <h1>Menu</h1>
+            <div class="col-8">
+                {{-- MENU  --}}
+                <div class="row">
+                    @foreach($dishes as $dish)
+                        <div class="card m-3 shadow" style="max-width: 500px;">
+                            <div class="row g-0">
+                                <div class="col-4">
+                                    <img src="{{ asset('storage/' . $dish->picture) }}" class="img-fluid rounded-start" alt="{{$dish->name}}">
+                                </div>
+                                <div class="col-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{$dish->name}}</h5>
+                                        <p class="card-text">{{ $dish->description}}</p>
+                                        <h5 class="card-title">prezzo</h5>
+                                        <p class="card-text">{{ $dish->price}}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach                
                 </div>
-            @endforeach                
+            </div>
+            <div class="col-4">
+                <div id="app">
+                    <Cart></Cart>
+                </div>
+            </div>
         </div>
-        <div id="app">
-            <Cart></Cart>
-        </div>
+        </div> 
     
-    </div>
+    <script src="{{ asset('js/app.js') }}"></script>    
 </body>
 </html>
     
