423877dc184b0f9ebbec9645cd1487520d26dcb2
ResminiLoris <resminiloris@gmail.com>
2021-11-12 19:59:35 +0100

    rimosso componente product da restaurant show

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -34,7 +34,20 @@
                 {{-- MENU  --}}
                 <div class="row">
                     @foreach($dishes as $dish)
-                        <product :dish="{{$dish}}"></product>
+                        <div class="card m-3 mt-0 shadow" style="max-width: 400px;">
+        <div class="row g-0">
+            <div class="col-4">
+                <img src="{{ asset('storage/' . $dish->picture) }}" class="img-fluid rounded-start" alt="{{$dish->name}}">
+            </div>
+            <div class="col-8">
+                <div class="card-body">
+                    <h5 class="card-title">{{$dish->name}}</h5>
+                    <p class="card-text m-0">{{$dish->description}}</p>
+                    <p class="card-text">{{ $dish->price . ' €'}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
                     @endforeach                
                 </div>
             </div>
