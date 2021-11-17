f491722e02b877fd059e15c2401cb8557a67a7fe
maldrek3103 <cpisanu96@gmail.com>
2021-11-17 12:38:54 +0100

    fixed registration

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -16,7 +16,7 @@ class RestaurantController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->except(['index', 'show', 'create']);
+        $this->middleware('auth')->except(['index', 'show', 'create', 'store']);
     }
 
     /**
