2c6e39c7b2d0c9d9ac2cf2d35147891e8a817067
FedericaDiChio <federica.dichio7@gmail.com>
2021-11-17 21:27:07 +0100

    some random animation

--- resources/views/orders/success.blade.php
+++ resources/views/orders/success.blade.php
@@ -6,12 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/css.css') }}" rel="stylesheet" type="text/css">
     <title>Success!</title>
 </head>
 
 <body>
-    <h1>Ordine avvenuto con successo</h1>
-    <a href="{{route('restaurants.index')}}" class="btn btn-primary rounded">Ritorna ai ristoranti</a>
+    <div class="container">
+        <div id="order-success" class="position-relative">
+            <h1 class="text-center" id="bau" >Ordine completato con successo!</h1>
+        </div>
+        <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+            <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>
+            <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+        </svg>
+        <div id="btn-return" class="text-center">
+            <a href="{{route('restaurants.index')}}" class="btn btn-primary rounded-pill">Torna ai ristoranti</a> 
+        </div>
+    </div>
 </body>
 
 </html>
