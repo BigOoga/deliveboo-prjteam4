05d4be33d33bfdcd2b6c62d6e7ffe6f24375badd
maldrek3103 <cpisanu96@gmail.com>
2021-11-17 17:36:07 +0100

    Better design on orders index and show

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -11,6 +11,10 @@
 
 <body>
     <div class="container mt-5 mb-5">
+        <div class="mb-3">
+            <a href="{{ route('orders.index') }}" class="btn btn-outline-primary">Torna all'indice</a>
+            <a href="{{ route('restaurants.dashboard') }}" class="btn btn-primary">Torna alla dashboard</a>
+        </div>
         <ul class="list-group list-unstyled">
             <div class="border p-5">
                 <li class="mb-2"><strong>ID: </strong>{{ $order->id }}</li>
@@ -21,20 +25,17 @@
                 <li class="mb-2"><strong>E-mail: </strong>{{ $order->email }}</li>
                 <li class="mb-2"><strong>Totale: </strong>{{ $order->total }}</li>
                 <li class="mb-2"><strong>Data: </strong>{{ $order->created_at }}</li>
-
-                {{-- tasto torna all'indice --}}
-                <li class="mb-2"><a href="{{ route('orders.index') }}" class="btn btn-outline-primary">torna
-                        all'indice</a></li>
+                <div class="d-flex py-3">
                 {{-- tasto edit --}}
-                <li class="mb-2"><a href="{{ route('orders.edit', $order->id) }}"
-                        class="btn btn-outline-warning">modifica</a></li>
+                <li class="me-2"><a href="{{ route('orders.edit', $order->id) }}"
+                        class="btn btn-outline-warning">Modifica</a></li>
                 {{-- tasto delete --}}
                 <form action="{{ route('orders.destroy', $order) }}" method="post">
                     @csrf
                     @method('DELETE')
-                    <button type="submit" class="btn btn-outline-danger mb-2">elimina</button>
-                    <a href="{{ route('restaurants.dashboard') }}" class="btn btn-primary">Torna alla dashboard</a>
+                    <button type="submit" class="btn btn-outline-danger">Elimina</button>
                 </form>
+            </div>
             </div>
         </ul>
     </div>
