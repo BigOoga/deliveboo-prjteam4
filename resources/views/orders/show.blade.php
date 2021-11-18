f102376994cee196018fb7127a79bfffd755836e
maldrek3103 <cpisanu96@gmail.com>
2021-11-18 15:53:10 +0100

    made show controller and blade

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -12,6 +12,7 @@
         </div>
         <ul class="list-group list-unstyled">
             <div class="border p-5">
+                <h2><strong>Dettagli ordine</strong></h2>
                 <li class="mb-2"><strong>ID: </strong>{{ $order->id }}</li>
                 <li class="mb-2"><strong>Nome: </strong>{{ $order->user_name }}</li>
                 <li class="mb-2"><strong>Cognome: </strong>{{ $order->user_surname }}</li>
@@ -20,6 +21,28 @@
                 <li class="mb-2"><strong>E-mail: </strong>{{ $order->email }}</li>
                 <li class="mb-2"><strong>Totale: </strong>{{ $order->total }}</li>
                 <li class="mb-2"><strong>Data: </strong>{{ $order->created_at }}</li>
+                <hr class="my-5">
+                <h2><strong>Piatti ordinati</strong></h2>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#ID</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Quantità</th>
+                            <th scope="col">Prezzo totale</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($dishes as $dish)
+                        <tr>
+                        <th scope="row">{{$dish->id}}</th>
+                        <td>{{$dish->name}}</td>
+                        <td>{{$dish->quantity}}</td>
+                        <td>{{$dish->price * $dish->quantity}}</td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
                 <div class="d-flex py-3">
                 </div>
             </div>
