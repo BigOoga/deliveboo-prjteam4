361f6ab6c853b21058dd78d7201f4bf49c183337
BigOoga <edoardo.ruffi@gmail.com>
2021-11-20 01:49:47 +0100

    beta statistic query

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -135,13 +135,14 @@ class OrderController extends Controller
     public function statistic()
     {
         $user_id = Auth::id();
-        $orders = DB::table('orders')
-            ->select('orders.*')
-            ->join('dish_order', 'dish_order.order_id', '=', 'orders.id')
-            ->join('dishes', 'dish_order.dish_id', '=', 'dishes.id')
-            ->join('restaurants', 'dishes.restaurant_id', '=', 'restaurants.id')
-            ->where('restaurants.id', $user_id)->groupBy('order_id')
-            ->get();
+
+        $orders = DB::select("SELECT MONTH(o.created_at), count(DISTINCT o.id) FROM `orders`as o
+        INNER JOIN dish_order as do ON do.order_id=o.id
+        INNER JOIN dishes as d on d.id=do.dish_id
+        WHERE d.restaurant_id = 14
+        GROUP BY month(o.created_at)");
+
+        dd($orders);
         return view('orders.statistic', compact('orders'));
     }
 }
