c56970908373e69aab3132fcc47f27f6ef1ba058
FedericaDiChio <federica.dichio7@gmail.com>
2021-11-21 12:29:01 +0100

    Added responsive restaurant pic

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -15,32 +15,34 @@
         <topbar></topbar>
         <div class="container pt-5">
             {{-- fixare il responsive  --}}
-            
             {{-- RESTAURANT DETAILS --}}
             <a class="btn mb-3" href="{{ route('restaurants.index') }}" role="button">Indietro</a>
-            <div class="row">
-                    
-                {{-- RESTAURANT INFO --}}
-                <div id="restaurant-infos" class="col-lg-8 col-md-6 p-3">
-                    <h1><strong>{{ $restaurant->name }}</strong></h1>
-                    <hr>
-                    <ul class="list-unstyled">
-                        <li><time><strong>Orario di apertura: </strong>{{ $restaurant->opening_time }}</time></li>
-                            <li><time><strong>Orario di chiusura: </strong>{{ $restaurant->closing_time }}</time></li>
-                            <li><strong>Contatti: </strong>{{ $restaurant->email }}</li>
-                            <li><strong>Spese di spedizione: € </strong>{{ $restaurant->delivery_fee }}</li>
-                    </ul> 
-                    {{-- INSERT DESCRIPTION --}}
-                    <h5><strong>Descrizione:</strong></h5>
-                    <p>{{ $restaurant->description }}</p>
-                </div>
-                {{-- RESTAURANT PIC --}}
-                <div id="restaurant-image" class="col-lg-4 col-md-6 d-flex align-items-center">
-                    <img src="{{ asset('storage/' . $restaurant->image) }}" alt="{{ $restaurant->name }}"
-                    class="img-fluid">
-                </div> 
-            </div>
-
+                <div class="row">
+                    <div class="card mb-3 p-0">
+                        {{-- RESTAURANT PIC --}}
+                        <div class="row g-0">
+                            <div class="col-lg-4 col-md-6 d-flex align-items-center">
+                                <img src="{{ asset('storage/' . $restaurant->image) }}" alt="{{ $restaurant->name }}" class="img-fluid rounded-start p-3">
+                            </div>
+                            <div class="col-lg-8 col-md-6 ">
+                                {{-- RESTAURANT INFO --}}
+                                <div class="card-body">
+                                <h1><strong>{{ $restaurant->name }}</strong></h1>
+                                <hr>
+                                <ul class="list-unstyled">
+                                    <li><time><strong>Orario di apertura: </strong>{{ $restaurant->opening_time }}</time></li>
+                                    <li><time><strong>Orario di chiusura: </strong>{{ $restaurant->closing_time }}</time></li>
+                                    <li><strong>Contatti: </strong>{{ $restaurant->email }}</li>
+                                    <li><strong>Spese di spedizione: € </strong>{{ $restaurant->delivery_fee }}</li>
+                                </ul>
+                                {{-- RESTAURANT DESCRIPTION --}}
+                                <h5><strong>Descrizione:</strong></h5>
+                                <p>{{ $restaurant->description }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>        
+        </div>           
             {{-- MENU --}}
             <div class="mt-5">
                 <h2>I nostri piatti</h2>
@@ -62,5 +64,5 @@
         color: white;
         font-size: 1rem;
         font-weight: bold;
-    };
+    }
 </style>
